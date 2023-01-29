84305b19111ca49c1b5cd53b717604805623f398
Mario <mario@supermario.dk>
2023-01-30 00:23:45 +0100

    Added js Logic and if winner is found

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
-let current_player = "x";
 LabelArray = [];
+input = [];
+let player = "x";
+let turn = 0;
+let row = 0;
+let col = 0;
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
 
-
-for (let i = 0; i<=9; i++){
+for (let i = 0; i<=8; i++){
     const box = document.getElementById("label-"+i.toString());
     LabelArray[i] = box;
 }
 
-console.log(LabelArray);
-for (let i = 0; i<=9; i++){
+for (let i in LabelArray){
     LabelArray[i].addEventListener("click", function(){
-        LabelArray[i].innerHTML = "x";
-    })
+        LabelArray[i].innerHTML = player;
+        options[i] =  player;
+        player = Change_player();
+        
+        for(let i = 0; i < winConditions.length; i++){
+            const condition = winConditions[i];
+            const cellA = options[condition[0]];
+            const cellB = options[condition[1]];
+            const cellC = options[condition[2]];
+        
+            if(cellA == "" || cellB == "" || cellC == ""){
+                continue;
+            }
+            if(cellA == cellB && cellB == cellC){
+                console.log("You won!")
+         }
+        }
+    });
 }
+
+
+
+function Change_player(){
+        if (turn == 1){
+            turn = 0
+            player = "x";
+        }
+        else{
+            turn = 1
+            player = "o";
+        }
+        return player;
+}
+
